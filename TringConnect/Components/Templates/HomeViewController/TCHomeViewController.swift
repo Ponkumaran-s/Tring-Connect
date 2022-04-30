dfb3510d0860e09985601f1fdde56674a321b31c
ponkumara.s <ponkumara.s@tringapps.com>
2022-04-30 18:23:41 +0530

    Folder stucture optimized and launch icon were added

---
+++ TringConnect/Components/Templates/HomeViewController/TCHomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TCHomeViewController.swift
+//  TringConnect
+//
+//  Created by Ponkumar's Mac on 30/04/22.
+//
+
+import UIKit
+
+class TCHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
