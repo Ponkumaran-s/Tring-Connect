60fa44bb08c919df2273ee103335b2c2126ed6fb
ponkumara.s <ponkumara.s@tringapps.com>
2022-05-02 00:08:57 +0530

    Removed URl constant

--- TringConnect/SupportingFiles/Constants/TCConstants.swift
+++ TringConnect/SupportingFiles/Constants/TCConstants.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 class TCConstants {
-    static let homeUrl = "https://b1cdc5c8-4df1-4b59-a583-81ba59ac4dc0.mock.pstmn.io/v1/home"
     static let addHighlightButton = "AddHighlight"
     static let notificationButton = "NotificationSelected"
     static let searchButton = "SearchSelected"
